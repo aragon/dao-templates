21ee930760d48ab057833fe2617aca141d4e40fd
Jorge Izquierdo <jorge@izqui.me>
2018-06-15 14:11:34 +0200

    Fix basekit indentation

--- kits/bare/contracts/KitBase.sol
+++ kits/bare/contracts/KitBase.sol
@@ -6,18 +6,18 @@ import "@aragon/os/contracts/lib/ens/ENS.sol";
 import "@aragon/os/contracts/lib/ens/PublicResolver.sol";
 
 contract KitBase {
-	ENS public ens;
+    ENS public ens;
     DAOFactory public fac;
 
     event DeployInstance(address dao);
     event InstalledApp(address appProxy, bytes32 appId);
 
     function KitBase(DAOFactory _fac, ENS _ens) {
-    	fac = _fac;
-    	ens = _ens;
+        fac = _fac;
+        ens = _ens;
     }
 
-	function latestVersionAppBase(bytes32 appId) public view returns (address base) {
+    function latestVersionAppBase(bytes32 appId) public view returns (address base) {
         Repo repo = Repo(PublicResolver(ens.resolver(appId)).addr(appId));
         (,base,) = repo.getLatest();
 
