5222b85581d38d425fa88de13823d2fd52a53d84
Jorge Izquierdo <jorge@izqui.me>
2018-08-31 08:54:35 +0200

    Exit script with callback

--- kits/bare/scripts/deploy-dao-factory.js
+++ kits/bare/scripts/deploy-dao-factory.js
@@ -1,6 +1,8 @@
 const daoFactoryMigration = require('@aragon/os/migrations/3_factory')
 
-module.exports = async () => {
-	const { daoFact } = await daoFactoryMigration(Promise.resolve(), null, null, artifacts)
-	console.log(daoFact.address)
+module.exports = async (callback) => {
+  const { daoFact } = await daoFactoryMigration(Promise.resolve(), null, null, artifacts)
+
+  console.log(daoFact.address)
+  callback()
 }
