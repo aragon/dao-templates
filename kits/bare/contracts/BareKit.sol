9b0e8dacc2e3d05e68547935b2e6c2ed609b3839
Jorge Izquierdo <jorge@izqui.me>
2018-09-04 23:55:55 +0200

    Update to latest aOS changes

--- kits/bare/contracts/BareKit.sol
+++ kits/bare/contracts/BareKit.sol
@@ -8,11 +8,11 @@ import "./KitBase.sol";
 contract BareKit is KitBase {
     function BareKit(DAOFactory _fac, ENS _ens) KitBase(_fac, _ens) {}
 
-    function newBareInstance() returns (Kernel dao, ERCProxy proxy) {
+    function newBareInstance() public returns (Kernel dao, ERCProxy proxy) {
         return newInstance(bytes32(0), new bytes32[](0), address(0), new bytes(0));
     }
 
-    function newInstance(bytes32 appId, bytes32[] roles, address authorizedAddress, bytes initializeCalldata) returns (Kernel dao, ERCProxy proxy) {
+    function newInstance(bytes32 appId, bytes32[] roles, address authorizedAddress, bytes initializeCalldata) public returns (Kernel dao, ERCProxy proxy) {
         address root = msg.sender;
         dao = fac.newDAO(this);
         ACL acl = ACL(dao.acl());
@@ -21,17 +21,13 @@ contract BareKit is KitBase {
 
         // If there is no appId, an empty DAO will be created
         if (appId != bytes32(0)) {
-            proxy = dao.newAppInstance(appId, latestVersionAppBase(appId)); 
-
-            if (initializeCalldata.length > 0) {
-                require(address(proxy).call(initializeCalldata));
-            }
+            proxy = dao.newAppInstance(appId, latestVersionAppBase(appId), initializeCalldata, false);
 
             for (uint256 i = 0; i < roles.length; i++) {
                 acl.createPermission(authorizedAddress, proxy, roles[i], root);
-            }        
+            }
 
-            InstalledApp(proxy, appId);
+            emit InstalledApp(proxy, appId);
         }
 
         acl.grantPermission(root, dao, dao.APP_MANAGER_ROLE());
