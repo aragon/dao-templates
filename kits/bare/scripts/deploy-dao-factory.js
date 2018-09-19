e57c12d3c52274b5f167674cd6ff97129d742dcc
Jorge Izquierdo <jorge@izqui.me>
2018-09-19 14:02:59 +0200

    Defer all deploying logic to JS script

--- kits/bare/scripts/deploy-dao-factory.js
+++
@@ -1,8 +0,0 @@
-const deployDAOFactory = require('@aragon/os/scripts/deploy-daofactory.js')
-
-module.exports = async (callback) => {
-  const { daoFactory } = await deployDAOFactory(null, { artifacts, verbose: false })
-
-  console.log(daoFactory.address)
-  callback()
-}
