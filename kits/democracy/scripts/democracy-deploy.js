1a6f9069974d4887307384245d12a860bae5e527
ßingen <bingentxu@gmail.com>
2018-11-05 22:24:26 +0100

    Beta: fix publish:rpc scripts

--- kits/democracy/scripts/democracy-deploy.js
+++ kits/democracy/scripts/democracy-deploy.js
@@ -1,10 +1,13 @@
 const deployKit = require('@aragon/kits-beta-base/scripts/deploy_kit.js')
 
+// Make sure that you have deployed ENS and APM and that you set the first one
+// in `ENS` env variable
 module.exports = async (callback) => {
   const deployConfig = {
-    kitName: 'DemocracyKit',
-    returnKit: true,
     artifacts,
+    kitName: 'democracy-kit',
+    kitContractName: 'DemocracyKit',
+    returnKit: true,
   }
 
   const { address } = await deployKit(null, deployConfig)
--- kits/multisig/scripts/multisig-deploy.js
+++ kits/multisig/scripts/multisig-deploy.js
@@ -1,10 +1,13 @@
 const deployKit = require('@aragon/kits-beta-base/scripts/deploy_kit.js')
 
+// Make sure that you have deployed ENS and APM and that you set the first one
+// in `ENS` env variable
 module.exports = async (callback) => {
   const deployConfig = {
-    kitName: 'MultisigKit',
-    returnKit: true,
     artifacts,
+    kitName: 'multisig-kit',
+    kitContractName: 'MultisigKit',
+    returnKit: true,
   }
 
   const { address } = await deployKit(null, deployConfig)
