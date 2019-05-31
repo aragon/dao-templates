6a66c1e990c6203939628fdfa741e1bc2cdbf9d1
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-05-31 15:41:56 -0300

    trust: add network to trust-deploy script

--- kits/trust/scripts/trust-deploy.js
+++ kits/trust/scripts/trust-deploy.js
@@ -1,10 +1,13 @@
 const deployKit = require('@aragon/kits-beta-base/scripts/deploy_kit.js')
 
-// Make sure that you have deployed ENS and APM and that you set the first one
-// in `ENS` env variable
+// Make sure that you have deployed ENS and APM and that you set the first one in `ENS` env variable
+
 module.exports = async (callback) => {
+  const network = process.argv[process.argv.findIndex(arg => arg === '--network') + 1]
+
   const deployConfig = {
     artifacts,
+    network,
     kitName: 'trust-kit',
     kitContractName: 'TrustKit',
     returnKit: true,
