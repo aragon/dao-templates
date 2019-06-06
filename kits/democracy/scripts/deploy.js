1e52adce89a590d56a959afea7f92e7305db1bdc
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-06-06 11:17:52 -0300

    democracy: fix scripts error handling

--- kits/democracy/scripts/deploy.js
+++ kits/democracy/scripts/deploy.js
@@ -4,7 +4,12 @@ const deploy_apm = require('@aragon/os/scripts/deploy-apm.js')
 const deploy_id = require('@aragon/id/scripts/deploy-beta-aragonid.js')
 const deploy_kit = require('@aragon/kits-beta-base/scripts/deploy_kit.js')
 
-module.exports = async (callback) => {
+const errorOut = (msg) => {
+  console.error(msg)
+  throw new Error(msg)
+}
+
+async function deploy() {
   console.log(`Deploying Democracy Kit, Owner ${process.env.OWNER}`)
 
   if (process.argv.length < 5) {
@@ -22,5 +27,9 @@ module.exports = async (callback) => {
   // aragonID
   await deploy_id(null, { artifacts, web3, ensAddress: ens.address })
 
-  await deploy_kit(null, { artifacts, kitName: 'democracy-kit', kitContractName: 'DemocracyKit', network: network, ensAddress: ens.address })
+  await deploy_kit(null, { artifacts, kitName: 'democracy-akropolis', kitContractName: 'DemocracyKit', network: network, ensAddress: ens.address })
+}
+
+module.exports = callback => {
+  deploy().then(() => callback()).catch(err => callback(err))
 }
