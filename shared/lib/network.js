888fb8b5a99cfb2cc559eccf2c026c4b112bb8d9
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-07-19 18:32:53 -0300

    shared: simplify network module export

--- shared/lib/network.js
+++ shared/lib/network.js
@@ -2,25 +2,28 @@ const DEFAULT_NETWORK = 'devnet'
 const LOCAL_NETWORKS = ['devnet', 'rpc']
 
 module.exports = web3 => {
-  const Network = {
-    async getNetworkId() {
-      return new Promise((resolve, reject) =>
-        web3.version.getNetwork((error, result) => error ? reject(error) : resolve(result))
-      )
-    },
 
-    async getNetworkName() {
-      const id = await Network.getNetworkId()
-      const { networks } = require('@aragon/os/truffle-config')
-      const networkName = Object.keys(networks).find(name => networks[name].network_id == id)
-      return networkName || DEFAULT_NETWORK
-    },
-
-    async isLocalNetwork() {
-      const networkName = await Network.getNetworkName()
-      return LOCAL_NETWORKS.includes(networkName)
-    },
+  async function getNetworkId() {
+    return new Promise((resolve, reject) =>
+      web3.version.getNetwork((error, result) => error ? reject(error) : resolve(result))
+    )
   }
 
-  return Network
+  async function getNetworkName() {
+    const id = await getNetworkId()
+    const { networks } = require('@aragon/os/truffle-config')
+    const networkName = Object.keys(networks).find(name => networks[name].network_id == id)
+    return networkName || DEFAULT_NETWORK
+  }
+
+  async function isLocalNetwork() {
+    const networkName = await getNetworkName()
+    return LOCAL_NETWORKS.includes(networkName)
+  }
+
+  return {
+    getNetworkId,
+    getNetworkName,
+    isLocalNetwork
+  }
 }
