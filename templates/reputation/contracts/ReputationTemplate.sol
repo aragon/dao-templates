76d703db5e241aa2c79dfdfc17218ab141e60d51
Alejandro J. Santander <palebluedot@gmail.com>
2019-07-25 11:18:50 -0600

    ReputationTemplate now accepts minAcceptanceQuorum as a parameter

--- templates/reputation/contracts/ReputationTemplate.sol
+++ templates/reputation/contracts/ReputationTemplate.sol
@@ -12,9 +12,7 @@ contract ReputationTemplate is BaseTemplate {
     uint8 constant private TOKEN_DECIMALS = uint8(18);
     uint256 constant private TOKEN_MAX_PER_ACCOUNT = uint256(0);
 
-    uint64 constant private ONE_PCT = uint64(1e16);                         // 1%
     uint64 constant private FINANCE_PERIOD = uint64(30 days);               // 30 days
-    uint64 constant private MIN_ACCEPTANCE_QUORUM = uint64(20 * ONE_PCT);   // 20%
 
     mapping (address => address) internal tokenCache;
 
@@ -33,10 +31,13 @@ contract ReputationTemplate is BaseTemplate {
         string _tokenName, 
         string _tokenSymbol,
         uint64 _voteDuration,
-        uint64 _supportRequired
-    ) public {
+        uint64 _supportRequired,
+        uint64 _minAcceptanceQuorum
+    ) 
+        public 
+    {
         newToken(_tokenName, _tokenSymbol);
-        newInstance(_id, _holders, _stakes, _voteDuration, _supportRequired);
+        newInstance(_id, _holders, _stakes, _voteDuration, _supportRequired, _minAcceptanceQuorum);
     }
 
     function newToken(string _name, string _symbol) public returns (MiniMeToken) {
@@ -45,7 +46,16 @@ contract ReputationTemplate is BaseTemplate {
         return token;
     }
 
-    function newInstance(string _id, address[] _holders, uint256[] _stakes, uint64 _voteDuration, uint64 _supportRequired) public {
+    function newInstance(
+        string _id, 
+        address[] _holders, 
+        uint256[] _stakes, 
+        uint64 _voteDuration, 
+        uint64 _supportRequired,
+        uint64 _minAcceptanceQuorum
+    ) 
+        public 
+    {
         require(_holders.length > 0, ERROR_EMPTY_HOLDERS);
         require(_holders.length == _stakes.length, ERROR_BAD_HOLDERS_STAKES_LEN);
         MiniMeToken token = _popTokenCache(msg.sender);
@@ -55,7 +65,7 @@ contract ReputationTemplate is BaseTemplate {
         Agent agent = _installDefaultAgentApp(dao);
         Finance finance = _installFinanceApp(dao, Vault(agent), FINANCE_PERIOD);
         TokenManager tokenManager = _installTokenManagerApp(dao, token, TOKEN_TRANSFERABLE, TOKEN_MAX_PER_ACCOUNT);
-        Voting voting = _installVotingApp(dao, token, _supportRequired, MIN_ACCEPTANCE_QUORUM, _voteDuration);
+        Voting voting = _installVotingApp(dao, token, _supportRequired, _minAcceptanceQuorum, _voteDuration);
 
         // Mint tokens
         _mintTokens(acl, tokenManager, _holders, _stakes);
--- templates/reputation/test/reputation.js
+++ templates/reputation/test/reputation.js
@@ -32,6 +32,7 @@ contract('Reputation', ([_, owner, holder1, holder2]) => {
   const TOKEN_SYMBOL = 'REP'
   const VOTE_DURATION = 60 * 60 * 24 * 7
   const SUPPORT_REQUIRED = 50e16
+  const MIN_ACCEPTANCE_QUORUM = 20e16
 
   before('fetch reputation template and ENS', async () => {
     const { registry, address } = await deployedAddresses()
@@ -51,17 +52,17 @@ contract('Reputation', ([_, owner, holder1, holder2]) => {
       context('when the creation fails', () => {
         if (creationStyle === 'single') {
           it('reverts when no holders were given', async () => {
-            await assertRevert(template.newTokenAndInstance.request(daoID, [], [], TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED), 'REPUTATION_EMPTY_HOLDERS')
+            await assertRevert(template.newTokenAndInstance.request(daoID, [], [], TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM), 'REPUTATION_EMPTY_HOLDERS')
           })
 
           it('reverts when holders and stakes length do not match', async () => {
-            await assertRevert(template.newTokenAndInstance.request(daoID, [holder1], STAKES, TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
-            await assertRevert(template.newTokenAndInstance.request(daoID, HOLDERS, [1e18], TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
+            await assertRevert(template.newTokenAndInstance.request(daoID, [holder1], STAKES, TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
+            await assertRevert(template.newTokenAndInstance.request(daoID, HOLDERS, [1e18], TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
           })
         } else if (creationStyle === 'separate') {
           context('when there was no token created before', () => {
             it('reverts', async () => {
-              await assertRevert(template.newInstance.request(daoID, HOLDERS, STAKES, VOTE_DURATION, SUPPORT_REQUIRED), 'REPUTATION_MISSING_TOKEN_CACHE')
+              await assertRevert(template.newInstance.request(daoID, HOLDERS, STAKES, VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM), 'REPUTATION_MISSING_TOKEN_CACHE')
             })
           })
 
@@ -71,12 +72,12 @@ contract('Reputation', ([_, owner, holder1, holder2]) => {
             })
 
             it('reverts when no holders were given', async () => {
-              await assertRevert(template.newInstance.request(daoID, [], [], VOTE_DURATION, SUPPORT_REQUIRED), 'REPUTATION_EMPTY_HOLDERS')
+              await assertRevert(template.newInstance.request(daoID, [], [], VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM), 'REPUTATION_EMPTY_HOLDERS')
             })
 
             it('reverts when holders and stakes length do not match', async () => {
-              await assertRevert(template.newInstance.request(daoID, [holder1], STAKES, VOTE_DURATION, SUPPORT_REQUIRED), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
-              await assertRevert(template.newInstance.request(daoID, HOLDERS, [1e18], VOTE_DURATION, SUPPORT_REQUIRED), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
+              await assertRevert(template.newInstance.request(daoID, [holder1], STAKES, VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
+              await assertRevert(template.newInstance.request(daoID, HOLDERS, [1e18], VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM), 'REPUTATION_BAD_HOLDERS_STAKES_LEN')
             })
           })
         }
@@ -85,11 +86,11 @@ contract('Reputation', ([_, owner, holder1, holder2]) => {
       context('when the creation succeeds', () => {
         before('create reputation entity', async () => {
           if (creationStyle === 'single') {
-            instanceReceipt = await template.newTokenAndInstance(daoID, HOLDERS, STAKES, TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED, { from: owner })
+            instanceReceipt = await template.newTokenAndInstance(daoID, HOLDERS, STAKES, TOKEN_NAME, TOKEN_SYMBOL, VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, { from: owner })
             tokenReceipt = instanceReceipt
           } else if (creationStyle === 'separate') {
             tokenReceipt = await template.newToken(TOKEN_NAME, TOKEN_SYMBOL, { from: owner })
-            instanceReceipt = await template.newInstance(daoID, HOLDERS, STAKES, VOTE_DURATION, SUPPORT_REQUIRED, { from: owner })
+            instanceReceipt = await template.newInstance(daoID, HOLDERS, STAKES, VOTE_DURATION, SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, { from: owner })
           }
 
           dao = Kernel.at(getEventArgument(instanceReceipt, 'DeployDao', 'dao'))
@@ -140,7 +141,7 @@ contract('Reputation', ([_, owner, holder1, holder2]) => {
         it('should have voting app correctly setup', async () => {
           assert.isTrue(await voting.hasInitialized(), 'voting not initialized')
           assert.equal((await voting.supportRequiredPct()).toString(), SUPPORT_REQUIRED)
-          assert.equal((await voting.minAcceptQuorumPct()).toString(), 20e16)
+          assert.equal((await voting.minAcceptQuorumPct()).toString(), MIN_ACCEPTANCE_QUORUM)
           assert.equal((await voting.voteTime()).toString(), VOTE_DURATION)
 
           await assertRole(acl, voting, voting, 'CREATE_VOTES_ROLE', tokenManager)
