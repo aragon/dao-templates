efe590d971aa10bc4e326404584d011054a9b447
Alejandro J. Santander <palebluedot@gmail.com>
2019-07-30 12:56:13 -0600

    Style refactoring if the abi.js helper module

--- shared/helpers/abi.js
+++ shared/helpers/abi.js
@@ -1,27 +1,26 @@
 const abi = require('web3-eth-abi')
 
-abiUtil = {
-
-  stringifyParams: (params) => {
-    return params.map(p => {
-      if (Array.isArray(p)) {
-        return abiUtil.stringifyParams(p)
+function stringifyParams(params) {
+  return params.map(p => {
+    if (Array.isArray(p)) {
+      return stringifyParams(p)
+    }
+    else {
+      if (typeof(p) === 'boolean') {
+        return p
       }
       else {
-        if (typeof(p) === 'boolean') {
-          return p
-        }
-        else {
-          return `${p}`
-        }
+        return `${p}`
       }
-    })
-  },
-
-  encodeFunctionCall: (funcSignature, types, params) => {
-    params = abiUtil.stringifyParams(params)
-    return abi.encodeFunctionSignature(funcSignature) + abi.encodeParameters(types, params).replace('0x', '');
-  }
+    }
+  })
 }
 
-module.exports = abiUtil
+function encodeFunctionCall(funcSignature, types, params) {
+  params = stringifyParams(params)
+  return abi.encodeFunctionSignature(funcSignature) + abi.encodeParameters(types, params).replace('0x', '');
+}
+
+module.exports = {
+  encodeFunctionCall
+}
