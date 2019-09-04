42dfdb7ed70d4530355f406bbbe50673a2ece172
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-09-04 10:48:52 +0200

    Chore: Update aragonOS to v4.3.0 (#152)
    * chore: update aragonOS to v4.3.0
    
    * tests: update gas cost tests

--- templates/trust/test/trust.js
+++ templates/trust/test/trust.js
@@ -121,8 +121,8 @@ contract('Trust', ([_, owner, beneficiaryKey1, beneficiaryKey2, heir1, heir2, mu
     })
 
     it('costs ~12.51e6 gas', async () => {
-      assert.isAtMost(prepareReceipt.receipt.gasUsed, 5.00e6, 'prepare script should cost almost 5.00e6 gas')
-      assert.isAtMost(setupReceipt.receipt.gasUsed, 5.67e6, 'setup script should cost almost 5.67e6 gas')
+      assert.isAtMost(prepareReceipt.receipt.gasUsed, 5e6, 'prepare script should cost almost 5.00e6 gas')
+      assert.isAtMost(setupReceipt.receipt.gasUsed, 5.7e6, 'setup script should cost almost 5.67e6 gas')
       assert.isAtMost(multiSigSetupReceipt.receipt.gasUsed, 1.85e6, 'multisig script should cost almost 1.85e6 gas')
     })
 
