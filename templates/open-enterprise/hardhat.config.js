b9320b9cf17f2935e60021f18f0ebafd66553704
yuetloo <contact@yuetloo.com>
2021-10-07 22:27:41 -0400

    use hardhat to publish template

--- templates/open-enterprise/hardhat.config.js
+++ templates/open-enterprise/hardhat.config.js
@@ -4,18 +4,24 @@ const config = require('@aragon/hardhat-config')
 config.solidity = {
   compilers: [
     {
-      version: "0.5.0"
+      version: "0.5.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
     },
     {
-      version: "0.4.24"
+      version: "0.4.24",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
     }
   ],
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 200,
-    },
-  },
 }
 
 module.exports = config
--- templates/open-enterprise/tasks/deploy-contract.js
+++ templates/open-enterprise/tasks/deploy-contract.js
@@ -1,7 +1,8 @@
 const { task } = require( 'hardhat/config' )
 
-const TASK_DEPLOY_CONTRACT = "deploy-contract"
-const TASK_COMPILE = "compile"
+const TASK_GET_CONSTRUCTOR_ARGS = 'verify:get-constructor-arguments'
+const TASK_DEPLOY_CONTRACT = 'deploy-contract'
+const TASK_COMPILE = 'compile'
 
 task(TASK_DEPLOY_CONTRACT, 'Deploy a contract')
   .addParam(
@@ -10,19 +11,33 @@ task(TASK_DEPLOY_CONTRACT, 'Deploy a contract')
     undefined,
     types.string
   )
+  .addOptionalParam(
+    "constructorArgsPath",
+    "File path to a javascript module that exports the list of arguments.",
+    undefined,
+    types.inputFile
+  )
   .addOptionalVariadicPositionalParam(
-    'constructorArgs',
-    'Constructor arguments for the contract.',
+    "constructorArgsParams",
+    "Contract constructor arguments. Ignored if the --constructor-args-path option is used.",
     []
   )
   .setAction(async (args, hre) => {
     console.log('compiling contracts...')
     await hre.run(TASK_COMPILE)
 
+    const {constructorArgsPath: constructorArgsModule, constructorArgsParams} = args
+    const constructorArgs = await run(TASK_GET_CONSTRUCTOR_ARGS, {
+      constructorArgsModule,
+      constructorArgsParams,
+    })
+
     console.log('creating contract...')
     const factory = await hre.ethers.getContractFactory(args.contract)
-    const deployment = await factory.deploy(...args.constructorArgs)
+    const deployment = await factory.deploy(...constructorArgs)
     const contractAddress = deployment.address
     await deployment.deployTransaction.wait()
     console.log('new contract address', contractAddress)
+    return contractAddress
   })
+
