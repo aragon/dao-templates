dc752258e5d521f8cf2cd97cd869c29a73262c3b
yuetloo <contact@yuetloo.com>
2021-10-08 16:19:54 -0400

    add hardhat

---
+++ shared/constructor-args/rinkeby.js
@@ -0,0 +1,14 @@
+/*
+ * Generic template contract contructor arguments
+ * for rinkeby
+ */
+module.exports = [  
+  // daoFactory address 
+  "0xad4d106b43b480faa3ef7f98464ffc27fc1faa96",
+  // ens address 
+  "0x98Df287B6C145399Aaa709692c8D308357bC085D",
+  // miniMeFactory address 
+  "0x6ffeb4038f7f077c4d20eaf1706980caec31e2bf", 
+  // aragonID address 
+  "0x3665e7bfd4d3254ae7796779800f5b603c43c60d"
+]
