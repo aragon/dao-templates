efe86f1b5c7e0849b60523cb22de97f26c262369
yuetloo <loo@aragon.org>
2021-10-19 11:58:07 -0400

    add hardhat (#183)

---
+++ shared/constructor-args/mainnet.js
@@ -0,0 +1,14 @@
+/*
+ * Generic template contract contructor arguments
+ * for mainnet
+ */
+module.exports = [
+  // daoFactory address 
+  "0xb9da44c051c6cc9e04b7e0f95e95d69c6a6d8031",
+  // ens address 
+  "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+  // miniMeFactory address 
+  "0x909d05f384d0663ed4be59863815ab43b4f347ec", 
+  // aragonID address 
+  "0x546aa2eae2514494eeadb7bbb35243348983c59d"
+]
