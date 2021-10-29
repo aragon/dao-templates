42ff1981aff430e891d5f0827d1ccc7af0705af7
yuetloo <contact@yuetloo.com>
2021-10-28 23:22:08 -0400

    move open-enterprise constructor args

---
+++ shared/constructor-args/harmony.js
@@ -0,0 +1,14 @@
+/*
+ * Generic template contract contructor arguments
+ * for harmony
+ */
+module.exports = [
+  // daoFactory address 
+  "0xc4ddbd0472c2688f5d278dafc02e66ee176c8de3",
+  // ens address 
+  "0x843ddfab8406e752d03fa75dbb275070f368658d",
+  // miniMeFactory address 
+  "0x96e54098317631641703404c06a5afad89da7373", 
+  // aragonID address 
+  "0x6115b7F05cCeF2D883AB64c88ba7e4BD1b877215"
+]
