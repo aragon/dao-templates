452215f3292c7a9e7aa775800903818234f17b3f
saransh <saranshisat@pm.me>
2021-11-05 08:44:28 +0530

    Added Fuji Network

--- templates/open-enterprise/truffle-config.js
+++ templates/open-enterprise/truffle-config.js
@@ -1,11 +1,11 @@
 // module.exports = require('@aragon/templates-shared/truffle.js')
 
-const config = require('@aragon/os/truffle-config')
+const config = require('@aragon/truffle-config-v4')
 
 const gasLimit = 7e6 - 1
 
 config.networks.rpc.gas = gasLimit
-config.networks.devnet.gas = gasLimit
+//config.networks.devnet.gas = gasLimit
 config.networks.rinkeby.gas = gasLimit
 config.networks.ropsten.gas = gasLimit
 config.networks.kovan.gas = gasLimit
