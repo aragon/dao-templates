0710acf3f933d095dcb27005d01c2630354b834c
Devendra Yadav <dev.koold@gmail.com>
2021-11-17 15:20:34 +0530

    removed commented line

--- templates/open-enterprise/truffle-config.js
+++ templates/open-enterprise/truffle-config.js
@@ -5,7 +5,6 @@ const config = require('@aragon/truffle-config-v4')
 const gasLimit = 7e6 - 1
 
 config.networks.rpc.gas = gasLimit
-//config.networks.devnet.gas = gasLimit
 config.networks.rinkeby.gas = gasLimit
 config.networks.ropsten.gas = gasLimit
 config.networks.kovan.gas = gasLimit
