0c8bcdc1bc7a3a96daa3d27078bdf7986d4452d5
Ramon Canales <emaildoramon@gmail.com>
2021-12-20 13:11:17 +0000

    Andromeda addresses

--- shared/constructor-args/andromeda.js
+++ shared/constructor-args/andromeda.js
@@ -4,11 +4,11 @@
  */
 module.exports = [
   // daoFactory address
-  "0xc4ddbd0472c2688f5d278dafc02e66ee176c8de3",
+  "0x6d4fb6ff01a172774f42789fcfcdd84e68c28494",
   // ens address
   "0x843ddfab8406e752d03fa75dbb275070f368658d",
   // miniMeFactory address
-  "0x7feA381D67eB8Fc513a64736C3E4C1e7FD2e9cFf", 
+  "0x7a62da7B56fB3bfCdF70E900787010Bc4c9Ca42e", 
   // aragonID address
-  "0x5ec029bE8A0d322a54bb9C672cc0feB7dDD5C88F"
+  "0xb5146fd572c669abc353902e43f47fda4609e38a"
 ]
