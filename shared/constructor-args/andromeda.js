8d407b034af4a757f4a9b32b5db0e2cb401ac3c5
yuetloo <contact@yuetloo.com>
2021-12-20 20:31:06 -0500

    update to new format

--- shared/constructor-args/andromeda.js
+++ shared/constructor-args/andromeda.js
@@ -1,14 +1,14 @@
 /*
- * Generic template contract contructor arguments
- * for bscTest
+ * Template contract contructor arguments
  */
-module.exports = [
+module.exports = {
   // daoFactory address
-  "0xc4ddbd0472c2688f5d278dafc02e66ee176c8de3",
+  daoFactory: "0xc4ddbd0472c2688f5d278dafc02e66ee176c8de3",
   // ens address
-  "0x843ddfab8406e752d03fa75dbb275070f368658d",
+  ensRegistry: "0x843ddfab8406e752d03fa75dbb275070f368658d",
   // miniMeFactory address
-  "0x7feA381D67eB8Fc513a64736C3E4C1e7FD2e9cFf", 
+  minimeFactory: "0x7feA381D67eB8Fc513a64736C3E4C1e7FD2e9cFf",
   // aragonID address
-  "0x5ec029bE8A0d322a54bb9C672cc0feB7dDD5C88F"
-]
+  aragonID: "0x5ec029bE8A0d322a54bb9C672cc0feB7dDD5C88F",
+  standardBounties: "TODO"
+}
