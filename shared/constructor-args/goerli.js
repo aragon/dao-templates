82729de733bb51a1a7082077ae4441103961cdeb
Mathias Scherer <scherer.mat@gmail.com>
2022-10-12 11:00:42 +0200

    feat: adds goerli for all templates

---
+++ shared/constructor-args/goerli.js
@@ -0,0 +1,15 @@
+/*
+ * Template contract contructor arguments
+ * for rinkeby
+ */
+module.exports = {
+  // daoFactory address 
+  daoFactory: "0x009e285a017f5cd0382dcd58a70fe5f255cc785c",
+  // ens address 
+  ensRegistry: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+  // miniMeFactory address 
+  minimeFactory: "0x1b33491848dc7d072b84e9a8d47420feeb7df725",
+  // aragonID address 
+  aragonID: "0xcf59c627b7a4052041c4f16b4c635a960e29554a",
+  standardBounties: "TODO"
+}
