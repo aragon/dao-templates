18a5490ea0220799fe05e2f928d21853a4923bba
Mathias Scherer <scherer.mat@gmail.com>
2022-10-12 15:15:48 +0200

    fix: minimeFactory address (#224)

--- shared/constructor-args/goerli.js
+++ shared/constructor-args/goerli.js
@@ -8,7 +8,7 @@ module.exports = {
   // ens address 
   ensRegistry: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
   // miniMeFactory address 
-  minimeFactory: "0x1b33491848dc7d072b84e9a8d47420feeb7df725",
+  minimeFactory: "0xC081540adF65F1Da5E1BC61f360EE4A9fEB0E1EF",
   // aragonID address 
   aragonID: "0xcf59c627b7a4052041c4f16b4c635a960e29554a",
   standardBounties: "TODO"
